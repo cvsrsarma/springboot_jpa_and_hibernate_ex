5b6876c46c103209f93fb60ff2ffda14eac6c610
cvsrsarma <vsrsarma.chinta@gmail.com>
2022-03-18 21:18:17 +0530

    Spring data jpa different findby method.

--- springboot_jpa_and_hibernate_ex/src/main/java/com/sb/orm/ex/service/sprngdatajpa/CourseSpringDataService.java
+++ springboot_jpa_and_hibernate_ex/src/main/java/com/sb/orm/ex/service/sprngdatajpa/CourseSpringDataService.java
@@ -31,8 +31,40 @@ public class CourseSpringDataService {
 		sortCourse();
 		paginationCourse();
 		
+		findByExamples();
+		likeExamples();
+
+		
 	}
 	
+	private void likeExamples() {
+		List<Course> c1	=cRepo.findByNameContaining("A");
+		List<Course> c12=cRepo.findByNameContainsOrderByNameDesc("A");
+		List<Course> c13=cRepo.findByNameIsContainingOrderById("A");
+		
+		logger.info("Find by Course name like--> {}",c1);
+		logger.info("Find by Course name like--> {}",c12);
+		logger.info("Find by Course name like--> {}",c13);
+	}
+
+	private void findByExamples() {
+		Optional<Course> cc= cRepo.findById(101582L);
+		String name= cc.get().getName();
+		List<Course> c2=cRepo.findByName(name);
+		logger.info("Find by Course name {}--> {}",name,c2);
+		long l=c2.get(0).getId();
+		List<Course> c1=cRepo.findByIdAndName(l, name);
+		logger.info("Find by Course name {} and id {} --> {}",name,l,c1);
+		List<Course> c3=cRepo.findByNameOrderByIdDesc(name);
+		logger.info("findByName Order By Id Desc name {}--> {}",name,c3);
+		long c4=cRepo.countByName(name);
+		logger.info("countByName EET2SLW--> {}",c4);
+		//List<Course> c5=cRepo.deleteByName(name);
+		//logger.info("Delete Course with name {}--> {}",name,c5);
+		Course c= cRepo.save(new Course(c2.get(0).getName()));
+		logger.info("Course saved with {}--> {}",name,c);
+	}
+
 	private void paginationCourse() {
 		long totalNumberOfRecoreds=cRepo.count();
 		logger.info("Total number of records {}",totalNumberOfRecoreds);
--- springboot_jpa_and_hibernate_ex/src/main/java/com/sb/orm/ex/step8/springdatajpa/CourseSprngDataRepo.java
+++ springboot_jpa_and_hibernate_ex/src/main/java/com/sb/orm/ex/step8/springdatajpa/CourseSprngDataRepo.java
@@ -13,5 +13,16 @@ public interface CourseSprngDataRepo extends JpaRepository<Course, Long>{
 	List<Course> findByIdBetween(Long l1, Long l2);
 
 	List<Course> findByIdBetweenOrderByNameAsc(long l, long m);
+	
+	List<Course> findByIdAndName(Long id, String name);
+	List<Course> findByName(String name);
+	List<Course> findByNameOrderByIdDesc(String name);
+	long countByName(String name);
+	List<Course> deleteByName(String name);
+	
+	//Like
+	List<Course> findByNameContaining(String Name);
+	List<Course> findByNameContainsOrderByNameDesc(String Name);
+	List<Course> findByNameIsContainingOrderById(String Name);
 
 }
